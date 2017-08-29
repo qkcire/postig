4250431be97a98f0fd63b08df49d8a0cf439ff71
qkcire <erickbquintanilla@gmail.com>
2017-08-28 21:58:57 -0700

    Added loader / disable screen functionality

--- window.js
+++ window.js
@@ -1,23 +1,27 @@
 // Run this function after the page has loaded
 $(function () {
-
   //Retreive login credentials
   $("#login-btn").on('click', function() {
     // function lock_and_auth()
     //  fadeTo screen + load feedback circle ring thing + lock all buttons on screen
     //  attempt authentication
     //  return boolean w/ yesAuth || noAuth
-    $(document.body).fadeTo(500, 0.55);
+
+    // fade designated columns instead of entire body
+    $("#orange").fadeTo(500, 0.55);
+    $("#green").fadeTo(500,0.55);
+
+    // all this works to activate loader
+    $("#preload").addClass("active"); //
+    $("#loader").css("pointer-events", "auto"); //
+    $("#spinner").css("visibility", "visible"); //
     console.log($("#user_name").val());
     console.log($("#pass_word").val());
-    //testing to see if its possible to disable buttons (spoiler: it isn't)
-    //refer to the following for some solutions:
-    //  https://stackoverflow.com/questions/1164635/how-to-enable-or-disable-an-anchor-using-jquery
-    //  https://stackoverflow.com/questions/2754546/can-i-disable-a-css-hover-effect-via-javascript
-    $("#user_name").prop("disabled", true);
-    $("#pass_word").prop("disabled", true);
-    $("#login-btn").prop("disabled", true);
-    $("#cancel-btn").prop("disabled", true);
+    
+    // don't need to explicity disable user & pword since loader takes precedence
+    // on screen and disables inherently
+    // $("#user_name").prop("disabled", true);
+    // $("#pass_word").prop("disabled", true);
   });
 
   //clear input data, sets to null, and unblurs fields on screen
