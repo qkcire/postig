0717e442058a71f9d081e352bda0f47b0adbdce0
qkcire <erickbquintanilla@gmail.com>
2017-08-30 23:16:41 -0700

    Add transition functionality & simple methods

--- window.js
+++ window.js
@@ -1,40 +1,59 @@
 // Run this function after the page has loaded
 $(function () {
+
   //Retreive login credentials
   $("#login-btn").on('click', function() {
-    // function lock_and_auth()
-    //  fadeTo screen + load feedback circle ring thing + lock all buttons on screen
-    //  attempt authentication
-    //  return boolean w/ yesAuth || noAuth
 
-    // fade designated columns instead of entire body
+    //fade tags title and logins
     $("#orange").fadeTo(500, 0.55);
     $("#green").fadeTo(500,0.55);
 
-    // all this works to activate loader!
+    //activate spinning loader  & disables buttons
     $("#preload").addClass("active"); //
     $("#loader").css("pointer-events", "auto"); //
     $("#spinner").css("visibility", "visible"); //
-    console.log($("#user_name").val());
-    console.log($("#pass_word").val());
-    setTimeout(transition_login_page, 1100);
 
-    // don't need to explicity disable user & pword since loader takes precedence
-    // on screen and disables inherently
-    // $("#user_name").prop("disabled", true);
-    // $("#pass_word").prop("disabled", true);
+    //authenticate user -- TO DO
+    $.getScript('./authenticate.js', function() {
+
+      // if SUCCESS transition into main page
+      if (test_auth_func($("#user_name").val(), $("#pass_word").val())) {
+        /// load page
+        setTimeout(function () {
+          transition_page();
+        }, 1500);
+      };
+    });
+    setTimeout(function() {
+      $("#roq").load("main-page.html");
+    }, 5200);
+
+    //now we want to empty out old html and load in new html
+
+
+
   });
-  function transition_login_page() {
+  function transition_page() {
+    transition_login_creds();
+    // setTimeout(transition_spin(), 400);
+  }
+  function transition_login_creds() {
     $("#red").removeClass("fadeIn");
     $("#red").css("-webkit-animation-delay", "0s");
     $("#red").addClass("fadeOut");
-    setTimeout(transition_post, 300);
-  }
+    setTimeout(transition_post, 1300);
+  };
 
   function transition_post() {
     $("#post").removeClass("fadeInUp");
     $("#post").addClass("fadeOutUp");
-  }
+    // setTimeout(transition_spin, 5500);
+  };
+
+  // function transition_spin() {
+  //   $("#spinner").addClass()
+  //   $("#spinner").css("visibility", "hidden");
+  // }
   //clear input data, sets to null, and unblurs fields on screen
   $("#cancel-btn").on('click', function() {
     $("#user_name").val(null).blur();
